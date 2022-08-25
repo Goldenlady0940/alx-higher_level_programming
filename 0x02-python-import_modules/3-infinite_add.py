85733550f045bfcd788283b9f02b10c7d1133044
Sebrina Abdulrezak <104963156+Goldenlady0940@users.noreply.github.com>
2022-08-25 21:08:25 +0300

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    value = 0
-    if len(sys.argv) == 1:
-        print(0)
-    else:
-        for i in range(1, len(sys.argv)):
-            value = value + int(sys.argv[i])
-        print(value)
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
