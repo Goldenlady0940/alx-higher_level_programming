5b66b27bafe36daa4bff4803aa47f494501e9e38
Goldenlady0940 <sebrinaa18@gmail.com>
2022-08-31 20:09:21 +0300

    task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    for i in range(len(my_list[])):
+        if my_list[i] == my_list[j]:
+            continue
+        else:
+            my_list[i] += my_list[j]
+    return my_list
