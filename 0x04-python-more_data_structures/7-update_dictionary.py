6cfd9205626d10e65b83178b90181bb5581ec163
Goldenlady0940 <sebrinaa18@gmail.com>
2022-08-31 21:39:43 +0300

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    new_dictionary = []
+    for i in range(len(a_dictionary)):
+        if a_dictionary[i] = key:
+            new_dictionary.append(value)
+        else:
+            new_dictionary.append(a_dictionary[i])
+        return new_dictionary
